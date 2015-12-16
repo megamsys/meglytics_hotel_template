2e2a4766cb1783fa1186fc2619ce8078c206ad77
Yeshwanth kumar <getyesh@megam.io>
2015-12-16 13:36:18 +0530

    usecase 3 works

--- src/main/scala/template.megam/Engine.scala
+++ src/main/scala/template.megam/Engine.scala
@@ -21,61 +21,34 @@ trait HotelTemplate extends spark.jobserver.SparkJob with spark.jobserver.NamedR
 
 object HotelAnalysisResult extends HotelTemplate {
 
- //usecase 1
 
   override def runJob(sc: SparkContext, config: Config) = {
+//usecase1
 
-    val dateRDD: RDD[String] = dateBuilder(sc)
+    val csvData: RDD[String] = parseData(sc)
+    val dateRDD: RDD[String] = dateBuilder(csvData)
     val yearRDD =dateRDD.distinct()
     val finalRDD=yearRDD.zipWithIndex
     val tmpRDD=finalRDD
-    finalRDD.foreach(println)
-  println(yearRDD)
-  //val r =dateRDD.map{case (x, iter) => (x, iter.toList)}
-//  println(r)
-//  println(count2(dateRDD.toList()))
-  println(dateRDD.asInstanceOf[AnyRef].getClass.getSimpleName)
-   //val newRDD = yearRDD.map{x=> dateRDD
-   //}
-//tmpRDD.groupBy(l => l).map(t => (t._1, t._2.length))
 
-    //dateRDD.foreach(println)
-  //val newRDD = yearRDD.filter { case (y) => y.endsWith("4")}
-  //val arrayList = scala.collection.mutable.MutableList[String]()
-//  val newRDD = dateRDD.map(x => x)
-  //val countMap = (dateRDD zip yearRDD) groupBy (identity) mapValues {_.length}
-  //val arrayList += finalRDD.zip(yearRDD).filter(x => x._1 != x._2).size
-   //finalRDD foreach{
-     //case(key) =>
-    //println("*********")
-    //println(key)
-    //println(key._1)
-//val arrayList += dateRDD.count(_ == key._1)
-  //val arrayList += dateRDD.groupBy(identity).mapValues(_.size)
+    //finalRDD.foreach(println)
+  //  println(yearRDD)
 
-  //}
-  //println(arrayList)
-   //newRDD.foreach(println)
-    //println(newRDD.count())
-    //val newRDD = dateRDD filter (a => )
-    //val regexpr = """[2000-2020]+""".r
-  //val newRDD = dateRDD.map { (y) => y}
-  //val newRDD = dateRDD.filter(row => row.contains("4")).count()
-   //val result= newRDD.count()
+//usecase3
+val services: scala.collection.Map[String, Long] = servicesPreferred(csvData)
+println(services)
 
-   //println(result)
-   //val yearList = scala.collection.mutable.MutableList[String]()
-   //val regexpr = """(/d/d/d/d)""".r
-  // val newRDD = dateRDD.map(_.)
-    //if (yearList != newRDD){
-     //yearList += newRDD
-     //println("*******************")
-     //println(newRDD)
-    //}
+println("========================")
+services.foreach(println)
 
+//usecase4
+val feebback = happyCustomers(csvData)
 
   }
-  def count2(letters: List[String]): Map[String, Int] = {
+
+
+
+def count2(letters: List[String]): Map[String, Int] = {
 var map = Map[String, Int]()
 for(letter <- letters) map += Pair(letter, map.getOrElse(letter, 0) + 1)
 map
--- src/main/scala/template.megam/HotelRDDData.scala
+++ src/main/scala/template.megam/HotelRDDData.scala
@@ -41,7 +41,13 @@ object Date {
 trait HotelRDDBuilder {
 
   //get ceph url from ENV/conf
-  val path = "/tmp/hotel_dataset.csv"
+  val path = "/home/yeshwanth/Downloads/hotel_dataset.csv"
+
+private def splitter(c: RDD[String]): List[Serializable] = {
+  val data = c.map(_.split(",").map(elem => elem.trim))
+  val header = new Formatter(data.take(1)(0))
+  return List(data, header)
+}
 
   private def getDates(c: RDD[String]): RDD[String] = {
 
@@ -52,13 +58,56 @@ trait HotelRDDBuilder {
     dates
   }
 
-  def dateBuilder(sc: SparkContext): RDD[String] = {
+
+private def getServices(c: RDD[String]): scala.collection.Map[String, Long] = {
+//returns a map of all list of services
+  val data = c.map(_.split(",").map(elem => elem.trim))
+  val header = new Formatter(data.take(1)(0))
+
+  val h_services = data.filter(line => header(line, "id") != "id").map(row => header(row, "room_services"))
+  h_services.countByValue
+}
+
+private def getFeedback(c: RDD[String]): scala.collection.Map[String, Long] = {
+
+  val data = c.map(_.split(",").map(elem => elem.trim))
+  val header = new Formatter(data.take(1)(0))
+
+  val feedback = data.filter(line => header(line, "id") != "id").map(row => header(row, "overall_feedback_score"))
+  feedback.countByValue
+
+}
+
+  def parseData(sc: SparkContext): RDD[String] = {
     val csvData = sc.textFile(path)
-    val dates = getDates(csvData)
+    return csvData
+  }
+  //uc1
+  def dateBuilder(d: RDD[String]): RDD[String] = {
+    val dates = getDates(d)
     dates.map(Date.eachRow).
       collect {
         case Some(d) => d.year
       }
+  }
+
+
+  //uc3
+  def servicesPreferred(d: RDD[String]): scala.collection.Map[String,Long] = {
+
+    val services = getServices(d)
+    return services
+  }
+
+//uc4
+   def happyCustomers(d: RDD[String]) = {
+     //take a list of scores - countbyValue
+     //get [1 to 5] count and [5 to 10] count
+     //1 - very poor, Hate this place, 2 - poor, i don think i ll come ever, 3 - ok(might return), 4- loved it,  5 - Hell yeah, I am coming here!
+
+     val fb = getFeedback(d)
+     println(fb)
+
   }
 
   def getHeaderdata(sc: SparkContext): RDD[Header] = {
@@ -69,6 +118,8 @@ trait HotelRDDBuilder {
         Header(id, name, phone, address, origin_country, room_type, checkin, checkout, roomservices, hotelservices, cost, method_payment, prevous_customer, overall_feedback_score)
     })
   }
+
+
 
 
 
