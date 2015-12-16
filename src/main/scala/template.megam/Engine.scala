16dd92ce238d59045c92e9f9321152c0f0c34c7b
LogeshEswar <ranjithar@megam.io>
2015-12-16 16:27:40 +0530

    run usecase2

--- src/main/scala/template.megam/Engine.scala
+++ src/main/scala/template.megam/Engine.scala
@@ -1,7 +1,6 @@
 package template.megam
 
-
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.{ Config, ConfigFactory }
 import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkContext, SparkContext._
@@ -9,48 +8,39 @@ import org.apache.spark.rdd.RDD
 import scala.util.Try
 import scala.collection.mutable.{ LinkedHashMap, ListBuffer }
 
-
-
-
-trait HotelTemplate extends spark.jobserver.SparkJob with spark.jobserver.NamedRddSupport with HotelRDDBuilder{
+trait HotelTemplate extends spark.jobserver.SparkJob with spark.jobserver.NamedRddSupport with HotelRDDBuilder {
 
   def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = spark.jobserver.SparkJobValid
 
 }
 
-
 object HotelAnalysisResult extends HotelTemplate {
 
-
   override def runJob(sc: SparkContext, config: Config) = {
-//usecase1
+    //usecase1
 
     val csvData: RDD[String] = parseData(sc)
-    val dateRDD: RDD[String] = dateBuilder(csvData)
-    val yearRDD =dateRDD.distinct()
-    val finalRDD=yearRDD.zipWithIndex
-    val tmpRDD=finalRDD
+    
+    val customer: scala.collection.Map[String, Long] = customercount(csvData)
+    println(customer)
+    println("=============================")
+    customer.foreach(println)
+    //  usecase2
+    val rooms: scala.collection.Map[String, Long] = roomPreferred(csvData)
+    println(rooms)
+    println("==============================")
+    rooms.foreach(println)
 
-    //finalRDD.foreach(println)
-  //  println(yearRDD)
+    //usecase3
+    val services: scala.collection.Map[String, Long] = servicesPreferred(csvData)
+    println(services)
 
-//usecase3
-val services: scala.collection.Map[String, Long] = servicesPreferred(csvData)
-println(services)
+    println("========================")
+    services.foreach(println)
 
-println("========================")
-services.foreach(println)
-
-//usecase4
-val feebback = happyCustomers(csvData)
+    //usecase4
+    val feebback = happyCustomers(csvData)
 
   }
 
-
-
-def count2(letters: List[String]): Map[String, Int] = {
-var map = Map[String, Int]()
-for(letter <- letters) map += Pair(letter, map.getOrElse(letter, 0) + 1)
-map
-}
 }
